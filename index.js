549c7ea37f034065ae1238509071484fde554e9f
Lucas Viccari <lviccari4@gmail.com>
2022-05-21 17:27:24 -0300

    Post

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
 const express = require("express");
 const app = express();
-const path = require ("path")
+const path = require("path");
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
 const pokedex = [
-
-    {
+  {
     id: 12,
-    nome:"Butterfree",
-    tipo:"Bug",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png"
-
-},
-{
+    nome: "Butterfree",
+    tipo: "Bug",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png",
+  },
+  {
     id: 4,
-    nome:"Charmander",
-    tipo:"Fire",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png"
-
-},
-{
-    id: 25 ,
-    nome:"Pikachu",
-    tipo:"Eletric",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png"
-
-}]
+    nome: "Charmander",
+    tipo: "Fire",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+  },
+  {
+    id: 25,
+    nome: "Pikachu",
+    tipo: "Eletric",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+  },
+  {
+    id: 7,
+    nome: "Squirtle",
+    tipo: "Water",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+  },
+];
+app.post("/add", (req, res) => {
+  const pokemon = req.body;
+  pokedex.push(pokemon);
+  res.redirect("/home");
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  
 });
 
 app.get("/home", (req, res) => {
-  res.render("index", {pokedex});
+  res.render("index", { pokedex });
 });
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000")
+
+
+
+
+
+
 );
